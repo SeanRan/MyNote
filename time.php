aea8fc52f808becfc1395ea6ce78b052df0d018a
SeanRan <seanqsran@gmail.com>
2016-10-02 19:41:22 +0800

    Update time.php

--- time.php
+++ time.php
@@ -1,12 +1,12 @@
 <?php
-date_default_timezone_set("Asia/Chongqing");
+date_default_timezone_set("PRC");
 /*做一个到程序员日的倒计时，距离目标时间大于一天时，以天计；仅有一天时，以小时计，节日当天，向浏览器输出happy1024*/
 $pf = strtotime("20161024");
 $now = time();
 // $now = strtotime("20161025"); this is a test line
 $difference = $pf-$now;
 if ($difference>86400) {
-	echo "距离程序元日还有".floor($difference/86400)."天";
+	echo "距离程序猿日还有".floor($difference/86400)."天";
 }else{
 	if($difference<=86400&&$difference>0){
 		$difh = floor($difference/3600);
